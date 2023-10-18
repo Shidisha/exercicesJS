f5b3ff17f396a8c7b628770f4b0c107900336d4f
Tshibangu <djo.tshibangu@kadea.local>
2023-10-18 20:14:52 +0100

    defined the function that return Hello a!

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 //1. Declare a variable firstname and initialize it with the value 'Lata'.
 let firstname = 'Lata';
+
 //2. Which value does x have after execution of the following code?
 //let x = 'Geeta';
 //the value of x will be 'Geeta'
+
 //3. Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'.
 let flower = 'rose';
 let tree = 'maple';
-console.log(tree);
+
+//4. Which value does x have after execution of the following code?
+/*let x = 'Tic';
+x = 'Tac';
+x = 'Toe';*/
+// x = 'Toe';
+
+/*5. Which value does x have after execution of the following code?                      let x = 'Laurel';
+let y = 'Hardy';
+let z = y;
+y = x;
+x = z;
+Hardy;*/
+
+//Define a function hello that returns 'Hello world!'
+function hello(){
+    return 'Hello word!';
+}
+
