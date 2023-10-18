57612327aa5cdc53487c309696f6d5ca285ed673
Tshibangu <djo.tshibangu@kadea.local>
2023-10-18 19:59:40 +0100

    definition de la fonction hello qui retourne un string Hello word

---
+++ index.js
@@ -0,0 +1,9 @@
+//1. Declare a variable firstname and initialize it with the value 'Lata'.
+let firstname = 'Lata';
+//2. Which value does x have after execution of the following code?
+//let x = 'Geeta';
+//the value of x will be 'Geeta'
+//3. Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'.
+let flower = 'rose';
+let tree = 'maple';
+console.log(tree);
